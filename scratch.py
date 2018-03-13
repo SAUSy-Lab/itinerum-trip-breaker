c717906b524928ec01a270efbcbc33f34dd354f0
Nate-Wessel <bike756@gmail.com>
2018-03-13 16:38:30 -0400

    added unproject function

--- scratch.py
+++ scratch.py
@@ -6,3 +6,12 @@ def project(longitude,latitude,projection_string='epsg:3347'):
 	outProj = Proj( init = projection_string )
 	x,y = transform( inProj, outProj, longitude, latitude )
 	return x,y
+
+
+def unproject(x,y,from_projection_string='epsg:3347'):
+	"""Unproject to lat-lon values. Default of 3347 is StatsCan Lambert."""
+	from pyproj import Proj, transform
+	inProj = Proj( init = from_projection_string )
+	outProj = Proj( init = 'epsg:4326' )
+	longitude,latitude = transform( inProj, outProj, x, y )
+	return longitude,latitude
