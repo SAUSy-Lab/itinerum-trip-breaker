1d3adacba1cd69b011bcac7f5c8ff4d5a8a92c02
Nate-Wessel <bike756@gmail.com>
2018-06-26 10:35:14 -0400

    import not needed within functions

--- misc_funcs.py
+++ misc_funcs.py
@@ -77,7 +77,6 @@ def project(longitude, latitude, projection_string='epsg:3347'):
 	Project lat-lon values. Default of 3347 is StatsCan Lambert.
 	Units in meters.
 	"""
-	from pyproj import Proj, transform
 	inProj = Proj(init='epsg:4326')
 	outProj = Proj(init=projection_string)
 	x, y = transform(inProj, outProj, longitude, latitude)
@@ -88,7 +87,6 @@ def unproject(x, y, from_projection_string='epsg:3347'):
 	"""
 	Unproject to lat-lon values. Default of 3347 is StatsCan Lambert.
 	"""
-	from pyproj import Proj, transform
 	inProj = Proj(init=from_projection_string)
 	outProj = Proj(init='epsg:4326')
 	longitude, latitude = transform(inProj, outProj, x, y)
