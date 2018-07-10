26c6d8d26b0ae26b1315dc309186d166ee71215b
Felipe Estevan Vicencio-Heap <felipe.estevan@gmail.com>
2018-07-09 20:05:29 -0500

    updated time.sh

--- etc/time.sh
+++ etc/time.sh
@@ -1,3 +1,3 @@
-cat config.py | grep multi_process > test.txt;
+cat config.py | grep multi_process > main_output.txt;
 git show -q >> test.txt;
-{ time python3 main.py >> test.txt & } 2>> test.txt; 
+{ time python3 main.py >> test.txt & } 2>> main_output.txt; 
