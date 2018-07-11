6ede883553d081c06aabe8162edd4b1fbeff1288
Felipe Estevan Vicencio-Heap <felipe.estevan@gmail.com>
2018-07-11 09:09:09 -0500

    changed paths

--- etc/isolate.py
+++ etc/isolate.py
@@ -1,6 +1,6 @@
-in_fd = "../inputs/SECcoordinates.csv"
+in_fd = "/inputs/SECcoordinates.csv"
 users = ["73fcaa05-20af-434b-a8d2-7d079ba35a51", "1EDF2943-E2C1-4608-8CD2-FA53A54C340E"]
-out = "../inputs/SECiso-coordinates.csv"
+out = "/inputs/SECiso-coordinates.csv"
 fd = open(in_fd, "r")
 out_fd = open(out, "w")
 line = fd.readline()
