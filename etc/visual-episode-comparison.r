5c7d6033ae860b327a2d5a12a45d41626e82059a
Nate-Wessel <bike756@gmail.com>
2018-08-01 12:29:53 -0400

    some minor tweaks

--- etc/visual-episode-comparison.r
+++ etc/visual-episode-comparison.r
@@ -1,3 +1,5 @@
+USER = 'A'
+
 d = read.csv('~/itinerum/itinerum-trip-breaker/outputs/episodes.csv')
 g = read.csv('~/itinerum/itinerum-trip-breaker/outputs/episodes_ground_truth.csv')
 # explicitly break into classes: unknown, travel, stationary
@@ -10,8 +12,8 @@ g[g$unknown=='True','class'] = 'unknown'
 d$class = factor(d$class)
 g$class = factor(g$class)
 # subset to me
-d = d[d$user_id == 'A',]
-g = g[g$user_id == 'A',]
+d = d[d$user_id == USER,]
+g = g[g$user_id == USER,]
 
 # get start and end times
 min_start_time = min( c( d$unix_start_time, g$unix_start_time ) )
@@ -20,22 +22,20 @@ g$st = (g$unix_start_time - min_start_time) / 3600
 d$et = d$st + c( diff(d$st), 0.1 )
 g$et = g$st + c( diff(g$st), 0.1 )
 
-cols = rainbow(3)
+cols = list(stationary='red',travel='green',unknown='blue')
 
-plot( 0, type='n', xlim=range(d$st), ylim=c(0,1) )
-for( i in 1:nrow(d)){
-	print(cols[d[i,'class']])
+plot( 0, type='n', main=USER, xlim=range(d$st), ylim=c(0,1), bty='n' )
+for( i in 1:nrow(d)){ # discovered is on top
 	lines(
 		x=c( d[i,'st'], d[i,'et'] ),
-		y=c( .5,.6 ),
-		col=cols[d[i,'class']]
+		y=c( .55, 1 ),
+		col=cols[[d[i,'class']]]
 	)
 }
-for( i in 1:nrow(g)){
-	print(cols[g[i,'class']])
+for( i in 1:nrow(g)){ # ground truth on bottom 
 	lines(
 		x=c( g[i,'st'], g[i,'et'] ),
-		y=c( .4,.5 ),
-		col=cols[g[i,'class']]
+		y=c( 0, .45 ),
+		col=cols[[g[i,'class']]]
 	)
 }
